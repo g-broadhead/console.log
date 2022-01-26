ac682d1d527ed6b874bcedda57aa229de30f1a26
gbroadhead <gwbroadhead@gmail.com>
2022-01-25 18:53:13 -0800

    models and routes in progress

---
+++ routes/postRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router()
+const { User, Post } = require('../models')
+const passport = require('passport')
+const jwt = require('jsonwebtoken')
+const { redirect } = require('express/lib/response')
+
+//user registration
+router.post('/users/register', (req, res) => {
+  User.register(new User({ username: req.body.username, email: req.body.email, dob: req.body.dob }), req.body.password, err => {
+    if (err) { console.log(err) }
+    res.sendStatus(200)
+    res.redirect('./loginPage.html')
+  })
+})
+
+// authenticate user login
+router.post('/users/login', (req, res) => {
+  User.authenticate()(req.body.username, req.body.password, (err, user) => {
+    if (err) { console.log(err) }
+    res.json(user ? {
+      username: user.username,
+      token: jwt.sign({ id: user.id }, process.env.SECRET)
+    } : null)
+  })
+})
+
+router.get('/users/profile', passport.authenticate('jwt'), (req, res) => res.json(req.user))
+
+module.exports = router
