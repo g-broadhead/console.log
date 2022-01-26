6560cd3bef6b3d6b3a57b2e4c3a14b3bf8ea71c3
gbroadhead <gwbroadhead@gmail.com>
2022-01-26 10:57:46 -0800

    update

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -6,7 +6,7 @@ const { redirect } = require('express/lib/response')
 
 //user registration
 router.post('/users/register', (req, res) => {
-  User.register(new User({ username: req.body.username, email: req.body.email, dob: req.body.dob }), req.body.password, err => {
+  User.register(new User({ username: req.body.username, email: req.body.email}), req.body.password, err => {
     if (err) { console.log(err) }
     res.sendStatus(200)
     res.redirect('./loginPage.html')
