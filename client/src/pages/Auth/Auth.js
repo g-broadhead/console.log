cad50d6f9750c3092055ccab68fdc319627d4894
Chanwoo Hwang <Chanwoo1017@hotmail.com>
2022-01-26 22:37:13 -0800

    update register page

--- client/src/pages/Auth/Auth.js
+++ client/src/pages/Auth/Auth.js
@@ -48,8 +48,7 @@ const Auth = () => {
     <div className="container">
       <div className="row bg-light p-5 rounded-lg m-3">
       <h1 className="display-4">Console.log</h1>
-      <p className="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex a inventore quis laboriosam, quae nihil. Veritatis, aut voluptatem! Minima consectetur, modi consequuntur qui neque expedita? Beatae omnis ipsa quos dolor.
-      Corrupti dolor repellat non accusamus nostrum necessitatibus, dicta libero. Dolorum blanditiis incidunt similique nesciunt laudantium est maiores deleniti consequatur debitis ipsam, animi eligendi alias suscipit error, doloremque facilis! Quod, saepe?</p>
+      <p className="lead">This is main Page</p>
       <hr className="my-4" />
     </div>
     <div className="row">
