f9a9ffeb6b6d36bad9dc6f03429fe8b07ea8d4ba
The Beast <johnnychavando5@gmail.com>
2022-01-27 11:17:00 -0800

    changes

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -110,13 +110,11 @@ export default function SignInSide() {
               </Button>
               <Grid container>
                 <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
+                  
                 </Grid>
                 <Grid item>
                   <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
+                    {"Register now"}
                   </Link>
                 </Grid>
               </Grid>
