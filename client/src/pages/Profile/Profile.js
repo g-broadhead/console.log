1fc832bbb3cbcb121d77fffc6aba378fe33fe187
gbroadhead <gwbroadhead@gmail.com>
2022-01-28 14:49:52 -0800

    updated useffects on profile page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -23,7 +23,15 @@ const Profile = (props) => {
   const [profileState, setProfileState] = useState({
     userData: {
       name: 'Test Name',
-      username: 'Test Username'
+      username: 'Test Username',
+      workname: 'Test Work',
+      positionTitle: 'QA Tester',
+      memberSince: '1/2022',
+      email: 'test@email.com',
+      github: 'github.com/test',
+      linkedIn: 'linkedin.com/test',
+      instagram: 'instagram.com/test',
+      twitter: 'twitter.com/test' 
     }
   })
 
@@ -40,41 +48,45 @@ const Profile = (props) => {
 
   return (
     <>
-    <AppHeader/>
-    <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={3}>
-      <Box gridColumn="span 5">
-        <Item>
-          <Stack direction="row" spacing={2} mt={3} alignItems="center"
-            justifyContent="center">
-            <Avatar
-              alt="Avatar"
-              src="/static/images/avatar/1.jpg"
-              sx={{ width: 200, height: 200 }}
-            />
-          </Stack>
-          <h1>Profile Name</h1>
-          <h2>Position Title</h2>
-          <h3>Works at: </h3>
-          <h3>Member Since: </h3>
-        </Item>
+      <AppHeader />
+      <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={3}>
+        <Box gridColumn="span 5">
+          <Item>
+            <Stack direction="row" spacing={2} mt={3} alignItems="center"
+              justifyContent="center">
+              <Avatar
+                alt="Avatar"
+                //  Add avatar get in src
+                src="/static/images/avatar/1.jpg"
+                sx={{ width: 175, height: 175 }}
+                // onclick event to make button. lead to modal for user input
+              />
+            </Stack>
+            <h1>{profileState.userData.name}</h1>
+            <h2>{profileState.userData.username}</h2>
+            <h2>{profileState.userData.positionTitle}</h2>
+            <h3>Works at: {profileState.userData.workname} </h3>
+            <h3>Member Since: {profileState.userData.memberSince}</h3>
+          </Item>
+        </Box>
+        <Box gridColumn="span 6">
+          <Item>
+            <h1>Posts</h1>
+            <div></div>
+          </Item>
+        </Box>
+        <Box gridColumn="span 5" gap={10} >
+          <Item>
+            <h1>Personal Information</h1>
+            <h3>Email: {profileState.userData.email}</h3>
+            <h3>Github: {profileState.userData.github}</h3>
+            <h3>LinkedIn: {profileState.userData.linkedIn}</h3>
+            <h3>Instagram: {profileState.userData.instagram}</h3>
+            <h3>Twitter: {profileState.userData.twitter}</h3>
+          </Item>
+        </Box>
+        <AppFooter />
       </Box>
-      <Box gridColumn="span 6">
-        <Item>
-        <h1>Posts</h1>
-        <div></div>
-        </Item>
-      </Box>
-      <Box gridColumn="span 5" gap={10} >
-      <Item>
-        <h1>Personal Information</h1>
-        <h3>Email: </h3>
-        <h3>Github: </h3>
-        <h3>LinkedIn: </h3>
-        <h3>Instagram: </h3>
-        <h3>Twitter: </h3>
-        </Item>
-      </Box>
-      <AppFooter />
     </>
   );
 }
