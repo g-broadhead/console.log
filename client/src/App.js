3cfbe95643dd5f699706aaaa87655d636c85da86
hvoyvng <jyu0628@gmail.com>
2022-01-28 14:14:31 -0800

    added admin routes and page

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Register from './pages/Register'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
 import Post from './pages/Post'
+import Admin from './pages/Admin'
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/register" element={<Register />} />
           <Route exact path="/login" element={<Login />} />
+          <Route exact path='/admin' element={<Admin />} />
         </Routes>
       </Router>
     </>
---
+++ client/src/pages/Admin/Admin.js
@@ -0,0 +1,65 @@
+import AppHeader from "../../components/AppHeader";
+import AppFooter from "../../components/AppFooter";
+import Container from "@mui/material/Container";
+import MainSidebar from "../../components/MainSidebar";
+import RightSidebar from "../../components/RightSidebar"
+import UserHomepage from "../../components/UserHomepage";
+import Box from '@mui/material/Box';
+import { spacing } from '@mui/system';
+import { flexbox } from '@mui/system';
+import Grid from '@mui/material/Grid';
+import { palette } from '@mui/system';
+
+const Home = (props) => {
+    const mainBox = {
+        display: "inline-flex",
+        width: "100%",
+        height: "100%",
+        justifyContent: 'space-between',
+    };
+
+    const sidebarStyle= {
+        // ml: 0,
+        margin: "auto",
+        bgcolor: "primary.main",
+    };
+
+    const rightbarStyle = {
+        // mr: 0,
+        margin: "auto",
+        bgcolor: "primary.main",
+    };
+    const homepageStyle = {
+        width: "100%",
+        margin: "auto",
+        mt: 4,
+
+    }
+
+    // const backgroundStyle= {
+    //     bgcolor: "warning.main",
+    //     // display: "block",
+    //     width: "100%",
+    //     height: "100%",
+    //     // position: "absolute"
+    // }
+    return (
+        <Box>
+            <Grid container spacing={1} sx={mainBox}>
+                <AppHeader />
+                <Grid item xs={2}>
+                    <MainSidebar sx={sidebarStyle}/>
+                </Grid>
+                <Grid item xs={8}>
+                    <UserHomepage sx={homepageStyle} />
+                </Grid>
+                <Grid item xs={2}>
+                <RightSidebar sx={rightbarStyle}/>
+                </Grid>
+                <AppFooter />
+            </Grid>
+        </Box>
+    );
+}
+
+export default Home;
