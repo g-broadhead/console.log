091ad0d0f710918ffbed859a42c0aca898030057
rytc <rytc@protonmail.com>
2022-01-28 16:31:37 -0800

    Tie posts to user

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -7,25 +7,30 @@ const { Post, User } = require('../models')
 // GET all posts
 // user must be logged in
 router.get('/post', passport.authenticate('jwt'), async function (req, res) {
-  const posts = await Post.findAll({ include: [User] })
-  res.json(posts)
+    const posts = await Post.findAll({ include: [User] })
+    res.json(posts)
 })
 
 // POST one post
-router.post('/post', passport.authenticate('jwt'), async function ({ body, user }, res) {
-  console.log(user);
-  const post = await Post.create({
-    content: body.content,
-    user: user.id
-  })
-  res.json(post)
+router.post('/post', passport.authenticate('jwt'), function ({ body, user }, res) {
+    console.log(user);
+    Post.create({
+        content: body.content,
+        user: user.id
+    }).then(post => {
+        User.findByIdAndUpdate(user._id, { $push: { posts: post._id } })
+            .then(update => {
+                res.json(post);
+            });
+
+    })
 })
 
 // DELETE one post
 //user must be logged in
 router.delete('/posts/:id', passport.authenticate('jwt'), async function ({ params: { id } }, res) {
-  await Post.destroy({ where: { id } })
-  res.sendStatus(200)
+    await Post.destroy({ where: { id } })
+    res.sendStatus(200)
 })
 
 module.exports = router
