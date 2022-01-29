d41dc192171dff9f3d8634ec8f11a65f7241ae07
gbroadhead <gwbroadhead@gmail.com>
2022-01-28 20:39:38 -0800

    edit pro modal added & filled out Post funct need

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,6 +1,7 @@
 import AppHeader from '../../components/AppHeader';
 import AppFooter from '../../components/AppFooter';
 import UserAPI from '../../utils/UserAPI';
+import * as React from 'react';
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from 'react'
@@ -14,6 +15,7 @@ import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import { Dialog } from '@mui/material';
 
 const Profile = (props) => {
   const Item = styled(Paper)(({ theme }) => ({
@@ -27,6 +29,10 @@ const Profile = (props) => {
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+  const [openProfile, setOpenProfile] = useState(false);
+  const profilehandleOpen = () => setOpenProfile(true);
+  const profilehandleClose = () => setOpenProfile(false);
+
   const ariaLabel = { 'aria-label': 'description' };
 
   const [profileState, setProfileState] = useState({
@@ -35,13 +41,13 @@ const Profile = (props) => {
       username: '',
       workname: 'Test Work',
       positionTitle: 'QA Tester',
-      memberSince: '1/2022',
+      memberSince: '',
       email: 'test@email.com',
       github: 'github.com/test',
       linkedIn: 'linkedin.com/test',
       instagram: 'instagram.com/test',
       twitter: 'twitter.com/test',
-      avatar: '',
+      avatar: ''
     }
   })
 
@@ -83,7 +89,7 @@ const Profile = (props) => {
       }).then(() => {
         setPostState({ ...postState, avatar: '' });
         handleClose();
-        window.location= '/profile';
+        window.location = '/profile';
       }).catch(err => {
         alert("Failed to upload avatar.");
       })
@@ -96,49 +102,73 @@ const Profile = (props) => {
   return (
     <>
       <AppHeader />
-      <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={3}>
-        <Box gridColumn="span 5">
+      <Box
+        display='grid'
+        gridTemplateColumns='repeat(12, 1fr)'
+        gap={3}
+      >
+        <Box gridColumn='span 5'>
           <Item>
-            <Stack direction="row" spacing={2} mt={3} alignItems="center"
-              justifyContent="center">
+            <Stack
+              direction='row'
+              spacing={2}
+              mt={3}
+              alignItems='center'
+              justifyContent='center'
+            >
               <Avatar
-                alt="Avatar"
+                alt='Avatar'
                 src={profileState.userData.avatar}
-                sx={{ width: 175, height: 175, border: "2px solid #000", cursor: 'pointer' }}
+                sx={{ width: 175, height: 175, border: '2px solid #000', cursor: 'pointer' }}
                 onClick={handleOpen}
               >{profileState.userData.name[0]}
               </Avatar>
 
               {/* Upload avatar modal */}
               <Modal
+                hideBackdrop
                 open={open}
                 onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
+                aria-labelledby='modal-modal-title'
+                aria-describedby='modal-modal-description'
               >
                 <Box sx={style}>
-                  <Typography id="modal-modal-title" variant="h6" component="h2">
+                  <Typography
+                    id='modal-modal-title'
+                    variant='h6'
+                    component='h2'
+                  >
                     Input Image URL to update Avatar
                   </Typography>
-                  <Box 
-                    component="form"
+                  <Box
+                    component='form'
                     sx={{
-                      '& > :not(style)': { m: 1, width: '25ch' },
+                      '& > :not(style)': { m: 1, width: '25ch' }
                     }}
                     noValidate
-                    autoComplete="off">
-                    <TextField 
-                      id="standard-basic" 
-                      label="Image URL" 
-                      variant="standard" 
-                      onChange={handlePostChange} 
+                    autoComplete='off'
+                  >
+                    <TextField
+                      id='standard-basic'
+                      label='Image URL'
+                      variant='standard'
+                      onChange={handlePostChange}
                       name='avatar'
-                      value={postState.avatar}/>
-                    <Button 
-                      type='submit' 
+                      value={postState.avatar}
+                    />
+                    <Button
+                      type='submit'
                       variant='outlined'
-                      onClick={handlePostSubmit}>
+                      onClick={handlePostSubmit}
+                    >
                       Submit
+                    </Button>
+                    <Button
+                      type='submit'
+                      variant='outlined'
+                      onClick={handleClose}
+                    >
+                      Close
                     </Button>
                   </Box>
                 </Box>
@@ -147,24 +177,69 @@ const Profile = (props) => {
             <h1>{profileState.userData.name}</h1>
             <h2>{profileState.userData.username}</h2>
             <h2>{profileState.userData.positionTitle}</h2>
-            <h3>Works at: {profileState.userData.workname} </h3>
-            <h3>Member Since: {profileState.userData.memberSince}</h3>
+            <h3>Works at: </h3> {profileState.userData.workname}
+            <h3>Member Since: </h3> {profileState.userData.memberSince}
           </Item>
         </Box>
-        <Box gridColumn="span 6">
+        <Box
+          gridColumn='span 6'
+        >
           <Item>
-            <h1>Posts</h1>
+            <h1>User Posts</h1>
             <div></div>
           </Item>
         </Box>
-        <Box gridColumn="span 5" gap={10} >
+        <Box
+          gridColumn='span 5'
+          gap={10}
+        >
           <Item>
             <h1>Personal Information</h1>
-            <h3>Email: {profileState.userData.email}</h3>
-            <h3>Github: {profileState.userData.github}</h3>
-            <h3>LinkedIn: {profileState.userData.linkedIn}</h3>
-            <h3>Instagram: {profileState.userData.instagram}</h3>
-            <h3>Twitter: {profileState.userData.twitter}</h3>
+            <h3>Email: </h3>{profileState.userData.email}
+            <h3>Github: </h3>{profileState.userData.github}
+            <h3>LinkedIn: </h3>{profileState.userData.linkedIn}
+            <h3>Instagram: </h3>{profileState.userData.instagram}
+            <h3>Twitter: </h3>{profileState.userData.twitter}
+            <>
+              <Button
+                onClick={profilehandleOpen}
+              >
+                Edit Profile
+              </Button>
+              <Modal
+                hideBackdrop
+                open={openProfile}
+                onClose={profilehandleClose}
+                aria-labelledby="child-modal-title"
+                aria-describedby="child-modal-description"
+              >
+                <Box sx={{ ...style, width: 200 }}>
+                  <h1 id="child-modal-title">Edit Profile</h1>
+                  <TextField id="worksAt" label="Works at" variant="standard" />
+                  <TextField id="jobTitle" label="Job Title" variant="standard" />
+                  <br></br>
+                  <h2>Personal Information</h2>
+                  <TextField id="email" label="Email" variant="standard" />
+                  <TextField id="gitHub" label="GitHub Url" variant="standard" />
+                  <TextField id="linkedIn" label="LinkedIn Url" variant="standard" />
+                  <TextField id="instagram" label="Instagram" variant="standard" />
+                  <TextField id="twitter" label="Twitter Url" variant="standard" />
+                  
+                  <Button
+                    type='submit'
+                    onClick={profilehandleClose}
+                  >
+                    Submit
+                  </Button>
+                  <Button
+                    type='submit'
+                    onClick={profilehandleClose}
+                  >
+                    Close
+                  </Button>
+                </Box>
+              </Modal>
+            </>
           </Item>
         </Box>
         <AppFooter />
