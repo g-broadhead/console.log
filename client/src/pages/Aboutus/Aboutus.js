606bb1643bff50ff6c1170db8e2e4188eab7a9f7
Harrison Reich <hreich1998@gmail.com>
2022-01-31 11:58:46 -0800

    imported item

--- client/src/pages/Aboutus/Aboutus.js
+++ client/src/pages/Aboutus/Aboutus.js
@@ -1,19 +1,32 @@
 import AppHeader from '../../components/AppHeader';
 import AppFooter from '../../components/AppFooter';
-import AboutusCard from '../../components/AboutusCard'
+import AboutusCard from '../../components/AboutusCard';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Item from '@mui/material/Item'
 
 
 const Aboutus = (props) => {
   return (
     <>
     <AppHeader />
-    <AboutusCard />
-    <AboutusCard />
-    <AboutusCard />
-    <AboutusCard />
-    <AboutusCard />
-    <AboutusCard />
-    {/* <AppFooter /> */}
+      <Box sx={{ flexGrow: 1, mt:'30px', mb:'30px' }}>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <Item>xs=8</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+          <Grid item xs={8}>
+            <Item>xs=8</Item>
+          </Grid>
+        </Grid>
+      </Box>
+    <AppFooter />
     </>
   )
 }
