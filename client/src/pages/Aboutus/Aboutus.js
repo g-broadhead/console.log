a1bd0582033e78fe3017783a0cb23dafcea79ab3
Harrison Reich <hreich1998@gmail.com>
2022-01-31 10:48:40 -0800

    code

--- client/src/pages/Aboutus/Aboutus.js
+++ client/src/pages/Aboutus/Aboutus.js
@@ -1,19 +1,11 @@
-import AppHeader from "../../components/AppHeader";
-import AppFooter from "../../components/AppFooter";
-import Container from "@mui/material/Container";
-import MainSidebar from "../../components/MainSidebar";
-import RightSidebar from "../../components/RightSidebar"
-import UserHomepage from "../../components/UserHomepage";
-import Box from '@mui/material/Box';
-import { spacing } from '@mui/system';
-import { flexbox } from '@mui/system';
-import Grid from '@mui/material/Grid';
-import { palette } from '@mui/system';
+import AppHeader from './components/AppHeader'
+import AppFooter from './components/AppFooter'
+
 
 const Aboutus = (props) => {
   return (
 
-  );
+  )
 }
 
 export default Aboutus;
