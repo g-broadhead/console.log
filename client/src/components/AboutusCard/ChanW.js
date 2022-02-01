201ffa8f044865561224bd0c769bcba2067d5065
Harrison Reich <hreich1998@gmail.com>
2022-02-01 11:46:53 -0800

    chanwoo linkedin

--- client/src/components/AboutusCard/ChanW.js
+++ client/src/components/AboutusCard/ChanW.js
@@ -25,7 +25,7 @@ const ChanwooH = (props) => {
           </Typography>
         </CardContent>
         <CardActions>
-          <Button href='' size="small">LinkedIn</Button>
+          <Button href='https://www.linkedin.com/in/nathan-hwang-b836ab117/' size="small">LinkedIn</Button>
           <Button href='https://github.com/adipure' size="small">Github</Button>
         </CardActions>
       </Card>
